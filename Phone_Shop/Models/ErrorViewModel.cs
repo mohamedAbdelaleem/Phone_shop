f14ec5aaed192147ae3048fac071bc8d371bc430
Mahmoud0-0Salah <mahmoudsalah2339@gmail.com>
2023-12-03 17:17:48 +0200

    delete

--- Phone_Shop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Phone_Shop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
