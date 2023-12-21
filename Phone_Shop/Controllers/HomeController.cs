d4a0742cd88de3051d4ac7a08aa72d71161a1e57
Mahmoud0-0Salah <mahmoudsalah2339@gmail.com>
2023-12-21 19:51:46 +0200

    Edit in some pages

--- Phone_Shop/Controllers/HomeController.cs
+++ Phone_Shop/Controllers/HomeController.cs
@@ -50,10 +50,6 @@ namespace Phone_Shop.Controllers
          
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Phone_Shop/Controllers/StoreController.cs
+++ Phone_Shop/Controllers/StoreController.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -28,9 +27,10 @@ namespace Phone_Shop.Controllers
         [Authorize(Roles = "Seller")]
         public IActionResult Create()
         {
-            var governoratesInEgypt = _context.Governorates.ToList();
-
+            var governoratesInEgypt = _context.Governorates;
+            var cityInEgypt = _context.Cities;
             ViewBag.GovernoratesInEgypt = governoratesInEgypt;
+            ViewBag.cityInEgypt = cityInEgypt;
             return View();
         }
 
@@ -49,65 +49,35 @@ namespace Phone_Shop.Controllers
             }
             catch
             {
-                var governoratesInEgypt = _context.Governorates.ToList();
-                return View();
-            }
-           
-        }
-
-        [Authorize(Roles = "Seller")]
-        public IActionResult Edit(int? id)
-        {
-            var governoratesInEgypt = _context.Governorates.ToList();
-            ViewBag.GovernoratesInEgypt = governoratesInEgypt;
-            var result = _context.Store.Find(id);
-            return View("Create", result);
-        }
-
-
-        [Authorize(Roles = "Seller")]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(Store model)
-        {
-
-            var sellerId = _userManager.GetUserId(User);
-            model.SellerId = sellerId;
-
-            try
-            {
-                _context.Store.Update(model);
-                _context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                var governoratesInEgypt = _context.Governorates.ToList();
-                ViewBag.GovernoratesInEgypt = governoratesInEgypt;
+                var governoratesInEgypt = _context.Governorates;
+                var cityInEgypt = _context.Cities;
                 return View();
             }
 
+
+
+
         }
 
-            [Authorize(Roles = "Seller")]
+        [Authorize(Roles = "Seller")]
         public IActionResult Delete(int? id)
         {
             var result = _context.Store.Find(id);
 
             if (result != null)
             {
-               var associatedProducts = _context.Product.Where(p => p.StoreId == id).ToList();
+                var associatedProducts = _context.Product.Where(p => p.StoreId == id).ToList();
 
                 if (associatedProducts.Count > 0)
                 {
                     ViewBag.ErrorMessage = "You must delete the associated products before deleting the store.";
                     return View();
                 }
-                
 
-                    _context.Store.Remove(result);
-                    _context.SaveChanges();
-                
+
+                _context.Store.Remove(result);
+                _context.SaveChanges();
+
             }
 
             return RedirectToAction(nameof(Index));
