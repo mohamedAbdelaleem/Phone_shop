46db6c00cc28ae4f8dce299c39d97f4926506a2f
unknown <mohamedkholeef082@gmail.com>
2023-12-24 21:45:32 +0200

    notification service

--- Phone_Shop/Services/NotificationService.cs
+++ Phone_Shop/Services/NotificationService.cs
@@ -1,4 +1,6 @@
-﻿using Phone_Shop.Data;
+﻿using Microsoft.CodeAnalysis;
+using Phone_Shop.Data;
+using Phone_Shop.Models;
 
 namespace Phone_Shop.Services
 {
@@ -6,11 +8,11 @@ namespace Phone_Shop.Services
     public interface INotification
     {
 
-        void SendProductAccepted(string userId, int productId);
-        void SendProductRejected(string userId, int productId);
+        public void SendProductAccepted(string userId, int productId);
+        public void SendProductRejected(string userId, int productId);
 
-        void SendOrderShipped(string userId, int orderId);
-        void SendOrderDelivered(string userId, int orderId);
+        public void SendOrderShipped(string userId, int orderId);
+        public void SendOrderDelivered(string userId, int orderId);
 
     }
 
@@ -21,5 +23,47 @@ namespace Phone_Shop.Services
             
             _context = context;
         }
+
+        public void SendProductAccepted(string userId, int productId) {
+            
+            var content = $"Your Product Application for {productId} has been Accepted!";
+            var notification = new Notification { UserId=userId, Content=content, IsReaded=false };
+            notification.CreatedAt = DateTime.Now;
+
+            _context.Notification.Add(notification);
+            _context.SaveChanges();
+
+            return;
+        }
+        public void SendProductRejected(string userId, int productId)
+        {
+            var content = $"Your Product Application for {productId} has been Rejected!";
+            var notification = new Notification { UserId = userId, Content = content, IsReaded = false };
+            notification.CreatedAt = DateTime.Now;
+
+            _context.Notification.Add(notification);
+            _context.SaveChanges();
+        }
+
+        public void SendOrderShipped(string userId, int orderId)
+        {
+            var content = $"Your Order (#{orderId}) has been Shipped!";
+            var notification = new Notification { UserId = userId, Content = content, IsReaded = false };
+            notification.CreatedAt = DateTime.Now;
+
+            _context.Notification.Add(notification);
+            _context.SaveChanges();
+        }
+        public void SendOrderDelivered(string userId, int orderId)
+        {
+            var content = $"Your Order (#{orderId}) has been Delivered!";
+            var notification = new Notification { UserId = userId, Content = content, IsReaded = false };
+            notification.CreatedAt = DateTime.Now;
+
+            _context.Notification.Add(notification);
+            _context.SaveChanges();
+        }
+
+
     }
 }
